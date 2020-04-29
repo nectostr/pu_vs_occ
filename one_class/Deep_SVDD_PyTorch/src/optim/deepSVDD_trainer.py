d0741b758e3f7760f1a9013e7eb3c80533776869
nectostr <bastinda96@gmail.com>
2020-04-29 16:18:18 +0300

    strange bug from dataset waaaa3

--- one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
+++ one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
@@ -149,8 +149,6 @@ class DeepSVDDTrainer(BaseTrainer):
         labels = np.array(labels)
         scores = np.array(scores)
 
-        self.test_mere = f1_score(labels, scores.round())
-        logger.info('Test set: {:.2f}%'.format(100. * self.test_mere))
         self.test_auc = roc_auc_score(labels, scores)
         logger.info('Test set auc: {:.2f}%'.format(100. * self.test_auc))
 
