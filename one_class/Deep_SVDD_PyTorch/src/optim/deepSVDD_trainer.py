f41d52154fe3ae85a13618acf37d01a373f6622e
nectostr <bastinda96@gmail.com>
2020-05-02 18:36:25 +0300

    lil bag after exp lr

--- one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
+++ one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
@@ -54,7 +54,7 @@ class DeepSVDDTrainer(BaseTrainer):
         # scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=self.lr_milestones, gamma=0.1)
         #scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 10)
         #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True)
-        scheduler = optim.ExponentialLR(optimizer, 0.1, last_epoch=-1)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.1, last_epoch=-1)
 
         # Initialize hypersphere center c (if c not loaded)
         if self.c is None:
