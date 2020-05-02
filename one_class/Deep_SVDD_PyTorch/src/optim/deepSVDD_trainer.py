b700939c41cb30217d6c8823e2307b6f89610370
nectostr <bastinda96@gmail.com>
2020-05-02 21:41:07 +0300

    new lambda in lr_lambda 2

--- one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
+++ one_class/Deep_SVDD_PyTorch/src/optim/deepSVDD_trainer.py
@@ -55,7 +55,7 @@ class DeepSVDDTrainer(BaseTrainer):
         #scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, 10)
         #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, verbose=True)
         #scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.7, last_epoch=50)
-        l1 = lambda epoch: (int(epoch >= 10) * self.lr/(epoch//10+1) + int(epoch < 10) * self.lr)**2
+        l1 = lambda epoch: (int(epoch >= 10) * self.lr/(epoch//10+1) + int(epoch < 10) * self.lr)**(1/2)
 
         scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=l1)
 
